3c4e476f881165f16b05fc5ba9ed8dc78a10b0e3
arkan-s <124037966+arkan-s@users.noreply.github.com>
2023-11-29 18:55:19 +0700

    Delete app directory,

--- app/server/databases/db_main.py
+++
@@ -1,9 +0,0 @@
-import motor.motor_asyncio
-
-MONGO_DETAILS = "mongodb://localhost:27017"
-
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
-database = client.KetPang
-
-# Menginisiasi collection(table)
-# komoditas_coll = database.get_collection("komoditas_collection")
